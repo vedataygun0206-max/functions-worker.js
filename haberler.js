2852d6702689c85910c5d91da746acb9fcfa433a
digitalgundem <vedataygun0206@gmail.com>
2026-08-03 09:17:21 +0300

    Add dynamic news card rendering to haberler.js
    Render news articles dynamically in the news grid.

--- haberler.js
+++ haberler.js
@@ -1,1 +1,27 @@
+const alan=document.querySelector(".news-grid");
 
+if(alan){
+
+alan.innerHTML="";
+
+haberler.forEach(h=>{
+
+alan.innerHTML+=`
+
+<div class="news-card">
+
+<img src="${h.resim}">
+
+<h3>${h.baslik}</h3>
+
+<p>${h.ozet}</p>
+
+<a href="#">Devamını Oku</a>
+
+</div>
+
+`;
+
+});
+
+}
