b5aa234b527b0cf9d26535abb2a097b71d4fe0ee
digitalgundem <vedataygun0206@gmail.com>
2026-08-03 09:16:13 +0300

    Add news articles data to data.js
    Added an array of news articles with titles, categories, summaries, and images.

--- data.js
+++ data.js
@@ -1,1 +1,27 @@
+const haberler = [
 
+{
+id:1,
+baslik:"Digital Gündem Yayında",
+kategori:"Gündem",
+ozet:"Türkiye'nin Dijital Rehberi yayına başladı.",
+resim:"https://picsum.photos/600/400?1"
+},
+
+{
+id:2,
+baslik:"81 İl Rehberi Hazırlanıyor",
+kategori:"Türkiye",
+ozet:"İller, ilçeler ve mahalleler sisteme eklenecek.",
+resim:"https://picsum.photos/600/400?2"
+},
+
+{
+id:3,
+baslik:"Firma Rehberi Yakında",
+kategori:"Firma",
+ozet:"Türkiye'nin işletmeleri tek çatı altında toplanacak.",
+resim:"https://picsum.photos/600/400?3"
+}
+
+];
